a15a4031ae247b59a0c4ed901f5acdfa510c764b
himanshu-raj-uk <himanshurajuk04@gmail.com>
2026-09-12 17:51:07 +0530

    Fix backend CORS configuration

--- Backend/main.js
+++ Backend/main.js
@@ -49,5 +49,5 @@ app.use(errorMiddleware);
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-  console.log(`Server Started Successfully on port ${PORT}`);
+  console.log("Server Started Successfully");
 });
