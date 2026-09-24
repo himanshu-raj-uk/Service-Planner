978987eaca404f7720840713d61fe9129e4d5db8
himanshu-raj-uk <himanshurajuk04@gmail.com>
2026-09-24 20:52:16 +0530

    Fix nodemailer email registration

--- Backend/Config/MailSender.js
+++ Backend/Config/MailSender.js
@@ -1,14 +1,16 @@
 const NodeSender = require("nodemailer");
 
 const Transporter = NodeSender.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.GOOGLE_USER_NAME,
     pass: process.env.GOOGLE_USER_PASSWORD,
   },
-  connectionTimeout: 15000,
-  greetingTimeout: 15000,
-  socketTimeout: 20000,
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 });
 
 const sendEmail = async ({ to, subject, html }) => {
